6f91cbdacb254df3bbb7d0d2a0854798fc9889ae
Anton Lenok <AILenok.SBT@sberbank.ru>
2016-11-20 21:56:23 +0300

    Init

---
+++ src/main/java/org/example/concurrency/dining/philosophers/MainApp.java
@@ -0,0 +1,16 @@
+package org.example.concurrency.dining.philosophers;
+
+/**
+ * @author Anton Lenok
+ * @since 20.11.16.
+ */
+public class MainApp {
+    private final static int COUNT_PHILOSOPHERS = 5;
+    private final static int COUNT_FORKS = COUNT_PHILOSOPHERS + 1;
+
+    public static void main(String[] args) {
+        System.out.println("Hello Dining philosophers");
+
+
+    }
+}
---
+++ src/main/java/org/example/concurrency/dining/philosophers/Philosopher.java
@@ -0,0 +1,11 @@
+package org.example.concurrency.dining.philosophers;
+
+/**
+ * @author Anton Lenok
+ * @since 20.11.16.
+ */
+public interface Philosopher extends Runnable {
+    void eat();
+
+    void think();
+}
